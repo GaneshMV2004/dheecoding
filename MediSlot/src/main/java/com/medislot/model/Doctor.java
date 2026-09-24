1af8e2732858ffbbc7128925408f493eb7d737b8
GaneshMV2004 <ganeshmv083@gmail.com>
2026-09-24 11:07:09 +0530

    Add MediSlot project: Doctor.java

---
+++ MediSlot/src/main/java/com/medislot/model/Doctor.java
@@ -0,0 +1,25 @@
+package com.medislot.model;
+
+public class Doctor {
+    private final int id;
+    private final String name;
+    private final String specialization;
+
+    public Doctor(int id, String name, String specialization) {
+        this.id = id;
+        this.name = name;
+        this.specialization = specialization;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+}
